a41dfcac709d9df86334bd3b1cad35b10935f2b0
VioletsOleander <1377232072@qq.com>
2025-04-01 11:18:59 +0800

    chore: add format_log_item.py

---
+++ format_log_item.py
@@ -0,0 +1,91 @@
+import argparse
+import re
+
+
+def setup_parser():
+    parser = argparse.ArgumentParser(description='Format log items.')
+    parser.add_argument('--no_confirm', action='store_true',
+                        help='Do not ask for confirmation before formatting each item')
+    parser.add_argument('--inplace', action='store_true',
+                        help='Format the log items in place')
+
+    return parser
+
+
+def open_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        return f.readlines()
+
+
+def write_file(file_path, content):
+    with open(file_path, 'w', encoding='utf-8') as f:
+        f.writelines(content)
+
+
+def ask_confirmation():
+    confirm = input(
+        "Do you want to format this item? (press enter to confirm, press any other key to refuse): ")
+    if confirm == '':
+        return True
+    else:
+        return False
+
+
+def format_log_item(contents, no_confirm):
+    doc_sec_re = re.compile(r'\\\[Doc\\\]\s')
+    paper_sec_re = re.compile(r'\\\[Paper\\\]\s')
+    item_re = re.compile(r'^- \[\[.+\]\]:?')
+
+    line_idx = 0
+    while line_idx < len(contents):
+        line = contents[line_idx]
+
+        if doc_sec_re.match(line):
+            print(f'In Doc section, line {line_idx+1}')
+            line_idx += 1
+
+            while line_idx < len(contents) and item_re.match(contents[line_idx]):
+                print(f'In line {line_idx+1}')
+                line = contents[line_idx]
+                print(line.strip())
+
+                part1, part2 = line.split('|')
+                prefix, path_part = part1.split('[[')
+                display_part, suffix = part2.split(']]')
+
+                if path_part[path_part.find('/')+1:] == display_part:
+                    print(
+                        f'Log item is already formatted')
+                else:
+                    print(f'Log item is not formatted')
+                    if no_confirm or ask_confirmation():
+                        display_part = path_part[path_part.find('/')+1:]
+                        formatted_line = f'{prefix}[[{path_part}|{display_part}]]{suffix}'
+                        contents[line_idx] = formatted_line
+                    print(
+                        f'The Formatted item is:\n{formatted_line.strip()}')
+
+                line_idx += 1
+
+        elif paper_sec_re.match(line):
+            pass
+        else:
+            pass
+
+        line_idx += 1
+
+    return contents
+
+
+if __name__ == '__main__':
+    parser = setup_parser()
+    args = parser.parse_args()
+
+    log_path = 'logs/Personal Log.md'
+    contents = open_file(log_path)
+
+    formatted_contents = format_log_item(contents, args.no_confirm)
+
+    store_path = log_path if args.inplace else 'logs/Formatted Personal Log.md'
+    write_file(store_path, formatted_contents)
+    print(f'Formatted log items saved to {store_path}')
